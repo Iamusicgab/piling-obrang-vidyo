5dbe0aaafc96280f04651baee2a2286e7bf1e30d
Gabriel Gonzales <gfgonzales@donbosco.edu.ph>
2026-03-11 21:25:37 +0800

    Analytics 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+	vite: {
+		plugins: [tailwindcss()],
+	},
 
-  integrations: [react()]
+	integrations: [react()],
+	output: "server",
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true, // set to false when using @vercel/analytics@1.4.0
+		},
+	}),
 });
